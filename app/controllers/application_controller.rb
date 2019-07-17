0930e24f77e587e721c3d6be70794235037ab604
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-07-17 14:08:53 -0700

    submit1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,22 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get'/' do 
+    @recipes = Recipe.All
+    erb :index
+  end
 
   # code actions here!
+  get '/recipes/new' do 
+    erb :new
+  end
 
+  post '/recipes' do 
+    @recipe = Recipe.create(params[:name],params[:ingredients])
+    redireict to "/recipes/#{@recipe.id}"
+  end
+
+   
+  
 end
